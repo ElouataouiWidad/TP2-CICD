3bcfdefeaecb639904b4b063b99e219f929bf4bb
ElouataouiWidad <98271360+ElouataouiWidad@users.noreply.github.com>
2025-09-25 11:06:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
-
 app.get('/', (req, res) => {
-  res.send('Hello from Container CI/CD 🚀');
+  res.send('Hello CI/CD with GitHub and Azure Container Apps!');
 });
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
 });
